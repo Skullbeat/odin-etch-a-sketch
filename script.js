a5c441518bb5d99a04c9f9fcb4047940c23ad2dc
Dani Gomez <d.gomezmartinez@icloud.com>
2023-09-11 20:08:56 +0200

    Select grid size
    Change grid size with an input slider

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
-const container = document.querySelector("#container-grid");
+// Grid size default
+const DEFAULT_SIZE = 16
 
-function generateGrid() {
-    for (let i = 0; i < 16 ; i++) {
+let currentSize = DEFAULT_SIZE
+
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+}
+
+const container = document.getElementById('container-grid');
+const sizeValue = document.getElementById('sizeValue');
+const sizeSlider = document.getElementById('sizeSlider');
+
+sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value);
+sizeSlider.onchange = (e) => changeSize(e.target.value);
+
+// Update the grid size with the slider input
+function changeSize(value) {
+    setCurrentSize(value);
+    updateSizeValue(value);
+    reloadGrid();
+}
+
+function updateSizeValue(value) {
+    sizeValue.innerHTML = `${value} x ${value}`;
+}
+
+function reloadGrid() {
+    clearGrid();
+    generateGrid(currentSize);
+}
+
+function clearGrid() {
+    container.innerHTML = '';
+}
+
+
+function generateGrid(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size ; i++) {
         const div = document.createElement("div");
         div.classList.add("grid-element");
         div.addEventListener("mouseover", function(){
@@ -11,5 +49,8 @@ function generateGrid() {
     }
 }
 
-generateGrid();
+// Generate the default grid when the page is loaded
+window.onload = () => {
+    generateGrid(DEFAULT_SIZE);
+}
   
