edb37526bbeaa71e0fc9e6cf075c8a80dfe30927
Dani Gomez <d.gomezmartinez@icloud.com>
2023-09-11 19:40:26 +0200

    Color on hover
    I paint in red the grid over which I pass the mouse over.

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ function generateGrid() {
     for (let i = 0; i < 16 ; i++) {
         const div = document.createElement("div");
         div.classList.add("grid-element");
+        div.addEventListener("mouseover", function(){
+            div.style.backgroundColor = 'red';
+        });
         container.appendChild(div);
     }
 }
